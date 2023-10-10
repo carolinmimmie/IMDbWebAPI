e93f63aba080bbb0d6ee85ff512b37fbec3945b2
carolinmimmie <carolinmimmie@gmail.com>
2023-10-10 12:58:13 +0200

    add ability to delete movie

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -16,14 +16,37 @@ public class MoviesController : ControllerBase
         this.context = context;
     }
 
-    // GET https://localhost:8000/movies
+    // GET https://localhost:8000/movies                 hämta alla fordon
+    // GET https://localhost:8000/Movies?Title=Clueless  hämta alla fordon av specifikt märke
     [HttpGet]
-    public IEnumerable<Movie> GetMovies()
+    public IEnumerable<Movie> GetMovies([FromQuery] string? title)//string? säger att den kan va null eller title
     {
-        var movies = context.Movie.ToList();
+        IEnumerable<Movie> movies;
+
+        if (string.IsNullOrEmpty(title))
+        {
+            movies = context.Movie.ToList();
+        }
+        else
+        {
+            movies = context.Movie.Where(x => x.Title == title);
+        }
 
         return movies;
     }
+
+   // GET https://localhost:8000/movies/{id}              hämta en film beronde på dess id
+    [HttpGet("{id}")]
+    public ActionResult<Movie> GetMovie(int id)
+    {
+        var movie = context.Movie.FirstOrDefault(x => x.Id == id);
+
+        if (movie is null)
+            return NotFound(); // 404 Not Found
+
+        return movie;
+    }
+
 
     // POST https://localhost:8000/movies
     // { "title": "", ... }
@@ -41,6 +64,29 @@ public class MoviesController : ControllerBase
         // Returnerar "201 Created", samt skickar även med en representation av filmen 
         // som skapades upp, som skicks som JSON till klienten
         // // { "id": 1, "title": "Aliens", ... }
-        return Created("", movie); 
+         return CreatedAtAction( // 201 Created // ge oss tillbaka extra info om vårat objekt 
+            nameof(GetMovie),
+            new { id = movie.Id },
+            movie);
+        //return Created("", movie); // om man inte vill skicka med någon locationheader - lämna den tom.. 
+    }
+        // DELETE http://localhost:8000/movies/1
+    [HttpDelete("{id}")] // kallas attribut
+    public IActionResult DeleteMovie(int id)
+    {
+        var movie = context.Movie.FirstOrDefault(x => x.Id == id);//Plocka ut fordon baserat på id
+
+        if (movie is null)// om fordonet inte finns
+        {
+            return NotFound(); // returnera 404 Not Found
+        }
+
+        context.Movie.Remove(movie);// om fordoner finns så vill vi radera
+
+        // Detta skickar en SQL DELETE till databashanteraren
+        // Exempelvis "DELETE FROM Vehicle WHERE Id = 1"
+        context.SaveChanges();
+
+        return NoContent(); // returnera204 No Content - 
     }
 }
